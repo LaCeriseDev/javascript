4d1d7dbba3c6245cf1bb5076c558c5eb8dd7eeb2
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-18 16:41:50 +0100

    Add getUserGender function to prompt user for gender input

--- 01_exo_synthese/js/01_exo_synthese.js
+++ 01_exo_synthese/js/01_exo_synthese.js
@@ -1,9 +1,11 @@
+// Demande à l'utilisateur de saisir son nom, puis son prénom ainsi que son genre
 let lastname = prompt("Votre nom");
 let firstname = prompt("Votre prénom");
-let sex = prompt("Tapez H si vous êtes un homme ou tapez si vous êtes une femme").toUpperCase();
+let sex = prompt(`Tapez "H" ou "F" si vous êtes un homme ou une femme`).toUpperCase();
 let dear = "";
 
 if (sex === "H") dear = "cher";
 else if (sex === "F") dear = "chère";
 
+// Affiche un message de bienvenue avec les informations personnels de l'utilisateur
 console.log(`Bonjour ${dear} ${firstname} ${name}, comment allez-vous ?`);
