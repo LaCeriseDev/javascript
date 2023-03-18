d330b9be0f1c9de5b5293a94e8ca9cd8c3bf2348
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-19 00:03:42 +0100

    riddle game finish

---
+++ 01_exo_synthese/js/riddle.js
@@ -0,0 +1,19 @@
+let min = 0;
+let max = 100;
+
+function getRandomNumberBetween(min,max) {
+    return Math.floor(Math.random()* (max-min + 1) + min);
+}
+
+let numberToFind = getRandomNumberBetween(min,max);
+let userGuessedNumber = 0;
+let tryToFind = 0;
+
+while (numberToFind != userGuessedNumber) {
+    userGuessedNumber = prompt(`Deviner le nombre entre ${min} et ${max}`);
+    if (userGuessedNumber > numberToFind ) console.log("plus bas");
+    if (userGuessedNumber < numberToFind ) console.log("plus haut");
+    tryToFind++;
+}
+
+console.log(`parfait le nombre était bien ${numberToFind}! Tu as trouvé en ${tryToFind}`);
