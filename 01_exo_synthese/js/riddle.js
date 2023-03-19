3a8f7d614c0b69326d2fee8cddb62df2d2b6679a
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-19 15:53:36 +0100

    fix issue where log doesn't display correctly

--- 01_exo_synthese/js/riddle.js
+++ 01_exo_synthese/js/riddle.js
@@ -1,11 +1,11 @@
-(() => {
-    const BOUNDARIES = { MIN: 0, MAX: 100 };
+(async () => {
+    const LIMITS = { MIN: 0, MAX: 100 };
     
     // Messages pour l'utilisateur
     const INVALID_INPUT_ERROR = "Entrée invalide. Veuillez réessayer.";
     const LOWER_GUESS = "Nombre essayé {input}, plus bas";
     const HIGHER_GUESS = "Nombre essayé {input}, plus haut";
-    const GUESS_PROMPT = `Deviner le nombre entre ${BOUNDARIES.MIN} et ${BOUNDARIES.MAX}`;
+    const GUESS_PROMPT = `Deviner le nombre entre ${LIMITS.MIN} et ${LIMITS.MAX}`;
     const SUCCESS = "Parfait le nombre était bien {numberToFind}! Tu as trouvé en {tryToFind} fois";
     
     // Expression de validation
@@ -33,24 +33,32 @@
      * @param {number} tryToFind - Le nombre d'essais effectués par l'utilisateur (par défaut 1)
      * @returns {void} - Rien n'est renvoyé, mais la fonction affiche un message dans la console en fonction du résultat
      */
-    const checkNumber = (numberToFind, tryToFind = 1) => {
-        setTimeout(() => {
-            const input = prompt(GUESS_PROMPT);
-            if (!isValidNumber(input) || input < BOUNDARIES.MIN || input > BOUNDARIES.MAX) {
+    const checkNumber = async (numberToFind, tryToFind = 1) => {
+            // const input = await new Promise(prompt(GUESS_PROMPT);
+            const input = await new Promise(resolve => {
+                setTimeout(() => resolve(prompt(GUESS_PROMPT)), 0);
+            });
+
+            // Si l'utilisateur clique sur "Cancel", affiche un message et termine la fonction.
+            if (input === null) {
+                console.log("Tu as abandonné. Le nombre à deviner était :", numberToFind);
+                return;
+            }
+
+            if (!isValidNumber(input) || input < LIMITS.MIN || input > LIMITS.MAX) {
                 alert(INVALID_INPUT_ERROR);
                 return checkNumber(numberToFind, tryToFind);
             }
             if (input != numberToFind) {
-                return input > numberToFind
-                    ? (console.log(LOWER_GUESS.replace("{input}", input)), checkNumber(numberToFind, tryToFind + 1))
-                    : (console.log(HIGHER_GUESS.replace("{input}", input)), checkNumber(numberToFind, tryToFind + 1));
+                const message = input > numberToFind ? LOWER_GUESS.replace("{input}", input) : HIGHER_GUESS.replace("{input}", input);
+                console.log(message);
+                return checkNumber(numberToFind, tryToFind + 1);
             }
             return console.log(SUCCESS.replace("{numberToFind}", numberToFind).replace("{tryToFind}", tryToFind));
-        }, 100);
     };
     
 
-    const numberToFind = getRandomNumberBetween(BOUNDARIES.MIN, BOUNDARIES.MAX);
+    const numberToFind = getRandomNumberBetween(LIMITS.MIN, LIMITS.MAX);
 
     checkNumber(numberToFind)
 })();
