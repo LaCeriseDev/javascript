13bfb7b20564024a5cc128c8e5e887a663533711
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-19 03:30:40 +0100

    add getRandomNumberBetween and checkNumber function with validation input and error handling

--- 01_exo_synthese/js/riddle.js
+++ 01_exo_synthese/js/riddle.js
@@ -1,5 +1,12 @@
 (() => {
     const BOUND = { MIN : 0, MAX : 100 };
+    const REGEX_VALID_NUMBER = /^[0-9]+$/;
+    const isValidNumber = number => { return REGEX_VALID_NUMBER.test(number); }
+    const INVALID_INPUT_ERROR = "Entrée invalide. Veuillez réessayer.";
+    const LOWER_MESSAGE = "Nombre essayé {input}, plus bas";
+    const HIGHER_MESSAGE = "Nombre essayé {input}, plus haut";
+    const GUESS_NUMBER = `Deviner le nombre entre ${BOUND.MIN} et ${BOUND.MAX}`;
+    const CONGRATULATION = "parfait le nombre était bien {numberToFind}! Tu as trouvé en {tryToFind} fois";
 
     /**
      * Renvoie un nombre aléatoire entre les bornes min et max incluses.
@@ -7,9 +14,29 @@
      * @param {number} max - La borne supérieure, incluse
      * @returns {number} - Un nombre aléatoire entre min et max, inclus
      */
-    const getRandomNumberBetween = (min, max) => {
-        return Math.floor(Math.random()*(max-min+1)-min);
-    }
+    const getRandomNumberBetween = (min, max) => { return Math.floor(Math.random()*(max-min+1)-min); };
 
-    getRandomNumberBetween(BOUND.MIN, BOUND.MAX);
+    /**
+     * Vérifie si le nombre entré par l'utilisateur est égal au nombre à deviner
+     * @param {number} numberToFind - Le nombre à deviner
+     * @param {number} tryToFind - Le nombre d'essais effectués par l'utilisateur (par défaut 1)
+     * @returns {void} - Rien n'est renvoyé, mais la fonction affiche un message dans la console en fonction du résultat
+     */
+    const checkNumber = (numberToFind, tryToFind= 1) => {
+        const input = prompt(GUESS_NUMBER);
+        if (!isValidNumber(input) || input < BOUND.MIN || input > BOUND.MAX) {
+            alert(INVALID_INPUT_ERROR);
+            return checkNumber(numberToFind,tryToFind);
+        }
+        if (input != numberToFind) {
+            return input > numberToFind 
+            ? (console.log(LOWER_MESSAGE.replace("{input}",input)), checkNumber(numberToFind, tryToFind+1))
+            : (console.log(HIGHER_MESSAGE.replace("{input}",input)), checkNumber(numberToFind, tryToFind+1));
+        }
+        return console.log(CONGRATULATION.replace("{numberToFind}",numberToFind).replace("{tryToFind}",tryToFind));
+    };
+
+    const numberToFind = getRandomNumberBetween(BOUND.MIN, BOUND.MAX);
+
+    checkNumber(numberToFind)
 })();
