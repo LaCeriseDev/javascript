8620f0d756a75c6a8194664864619a3e697abcf5
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-20 09:28:00 +0100

    add what_is_number.js

---
+++ 03_variation/js/what_is_number.js
@@ -0,0 +1,9 @@
+let number = prompt("Saisissez un nombre")
+
+if ( number > 0 ) { console.log("votre nombre est positif");}
+else if ( number > 0 ) { console.log("votre nombre est négatif");}
+else { console.log("votre nombre est null");}
+
+if ( number > 0 && number < 100 ) { console.log("votre nombre est entre 0 et 100"); }
+else if ( number < 0 ) { console.log("votre nombre est négatif"); }
+else { console.log("votre nombre est supérieur à 100"); }
