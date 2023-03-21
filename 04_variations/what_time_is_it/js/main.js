8391aa341f8e21f6718538babe5e2760b7a34014
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-21 09:58:34 +0100

    refactor

--- 04_variations/what_time_is_it/js/main.js
+++ 04_variations/what_time_is_it/js/main.js
@@ -13,6 +13,8 @@ const TIME = {
  */
 const incrementTimebyOneSecond = (hours, minutes, seconds) => { 
     if (hours === 24) hours = 0;
+    if (hours > 24) hours = hours - 24;
+
     seconds += 1;
     if (seconds >= 60) { seconds = 0; minutes += 1;
         if (minutes >= 60) { minutes = 0; hours += 1;
