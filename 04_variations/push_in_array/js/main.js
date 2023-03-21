d1c0b52791352beb3fe0fa8d18aa5f099b780c77
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-21 09:22:49 +0100

    refactor

--- 04_variations/push_in_array/js/main.js
+++ 04_variations/push_in_array/js/main.js
@@ -1,3 +1,8 @@
+/**
+ * Ajoute des mots au tableau `wordList` jusqu'à ce que l'utilisateur saisisse "stop".
+ * @param {Array<string>} wordList - Un tableau de mots à remplir (facultatif, par défaut un tableau vide).
+ * @returns {Array<string>}- Le tableau de mots rempli par l'utilisateur.
+ */
 const addWord = (wordList = []) => {
     let word = prompt("Un mot à rajouter ?").toLowerCase();
     return word === "stop" ? wordList  : (wordList.push(word), addWord(wordList));
--- 04_variations/three_musketeers_or_four/js/main.js
+++ 04_variations/three_musketeers_or_four/js/main.js
@@ -1,3 +1,10 @@
+/**
+ * Énumère les mousquetaires dans une chaîne de caractères, en les séparant par des virgules.
+ * @param {Array<string>} array - Un tableau contenant les noms des mousquetaires.
+ * @param {string} [str=""] - La chaîne de caractères accumulée au fil des appels récursifs (laisser vide lors de l'appel initial).
+ * @param {number} [i=0] - L'index du mousquetaire actuel dans le tableau (laisser à 0 lors de l'appel initial).
+ * @returns {string} - La chaîne de caractères contenant les noms des mousquetaires séparés par des virgules.
+ */
 const enumMusketeers = (array , str= "", i = 0) => {
     if( i === 0 ) str += array[i]
     else str += ', ' + array[i]
