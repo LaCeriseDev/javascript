01a9f64dad4535a39fb8024c3fd6dd882bed4f74
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-22 14:15:21 +0100

    branche pédagogue

--- 01_exo_synthese/personalized_welcome/js/main.js
+++ 01_exo_synthese/personalized_welcome/js/main.js
@@ -1,15 +1,18 @@
 // Demande à l'utilisateur de saisir son nom, puis son prénom ainsi que son genre
-let lastname = prompt("Votre nom");
-let firstname = prompt("Votre prénom");
-let gender = "";
-// let dear = gender === 'H' ? "cher" : "chère";
-let dear = "";
+let nom = prompt("Votre nom");
+let prenom = prompt("Votre prénom");
+let genre = "";
+// let dear = genre === 'H' ? "cher" : "chère";
+let cher = "";
 
-while (gender !== 'H' && gender !== 'F') {
-    gender = prompt(`Tapez "H" ou "F" si vous êtes un homme ou une femme`).toUpperCase();
-    if (gender === "H") dear = "cher";
-    else if (gender === "F") dear = "chère";
+
+while (genre !== 'H' && genre !== 'F') {
+    genre = prompt(`Tapez "H" ou "F" si vous êtes un homme ou une femme`).toUpperCase();
 }
 
+
+if (genre === "H") dear = "cher";
+else if (genre === "F") dear = "chère";
+
 // Affiche un message de bienvenue avec les informations personnels de l'utilisateur
-console.log(`Bonjour ${dear} ${firstname} ${lastname}, comment allez-vous ?`);
+console.log(`Bonjour ${dear} ${prenom} ${nom}, comment allez-vous ?`);
--- 01_exo_synthese/riddle/js/main.js
+++ 01_exo_synthese/riddle/js/main.js
@@ -1,18 +1,17 @@
-function getRandomNumberBetween(min,max) {
-    return Math.floor(Math.random()* (max-min + 1) + min);
-}
-
 let min = 0;
 let max = 100;
-let numberToFind = getRandomNumberBetween(min,max);
-let userGuessedNumber = 0;
-let tryToFind = 0;
+let nombreATrouver = Math.floor(Math.random()* (max-min + 1) + min);
+let nombreUtilisateur = 0;
+let prenom;
+// let essai = 0;
 
-while (numberToFind != userGuessedNumber) {
-    userGuessedNumber = prompt(`Deviner le nombre entre ${min} et ${max}`);
-    if (userGuessedNumber > numberToFind ) {console.log("plus bas");}
-    if (userGuessedNumber < numberToFind ) {console.log("plus haut");}
-    tryToFind++;
+while (nombreATrouver != nombreUtilisateur) {
+
+    nombreUtilisateur = prompt(`Deviner le nombre entre ${min} et ${max}`);
+
+    if (nombreUtilisateur > nombreATrouver ) {console.log("plus bas");}
+
+    else if (nombreUtilisateur < nombreATrouver ) {console.log("plus haut");}
 }
 
-console.log(`parfait le nombre était bien ${numberToFind}! Tu as trouvé en ${tryToFind}`);
+console.log(`parfait le nombre était bien ${nombreATrouver}!`);
