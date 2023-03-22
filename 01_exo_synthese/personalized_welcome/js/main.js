8a757980e6161cbadd65d8e2a281d6807d16a578
LaCeriseDev <zaniniromain.pro@gmail.com>
2023-03-22 13:13:20 +0100

    🎨   Improve

--- 01_exo_synthese/personalized_welcome/js/main.js
+++ 01_exo_synthese/personalized_welcome/js/main.js
@@ -1,12 +1,15 @@
 // Demande à l'utilisateur de saisir son nom, puis son prénom ainsi que son genre
 let lastname = prompt("Votre nom");
 let firstname = prompt("Votre prénom");
-let gender = prompt(`Tapez "H" ou "F" si vous êtes un homme ou une femme`).toUpperCase();
+let gender = "";
 // let dear = gender === 'H' ? "cher" : "chère";
 let dear = "";
 
-if (gender === "H") dear = "cher";
-else if (gender === "F") dear = "chère";
+while (gender !== 'H' && gender !== 'F') {
+    gender = prompt(`Tapez "H" ou "F" si vous êtes un homme ou une femme`).toUpperCase();
+    if (gender === "H") dear = "cher";
+    else if (gender === "F") dear = "chère";
+}
 
 // Affiche un message de bienvenue avec les informations personnels de l'utilisateur
 console.log(`Bonjour ${dear} ${firstname} ${lastname}, comment allez-vous ?`);
